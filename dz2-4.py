74344d55a8771910b74092113e161cd2406d261a
AndreyLyamtsev <82473051+AndreyLyamtsev@users.noreply.github.com>
2021-06-17 22:30:30 +0300

    Delete dz2-4.py

--- dz2-4.py
+++
@@ -1,9 +0,0 @@
-sentence = input("Введите слова, разделенные пробелами:")
-separate_words = sentence.split()
-n = 0
-for el in separate_words:
-    n = n + 1
-    if len(el) <= 10:
-        print(n, el)
-    else:
-        print(n, el[0:10])
